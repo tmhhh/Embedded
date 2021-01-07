bc8544ea936ee75bda307625918934e1a1f4abbe
LeQuangHuy <huyle29092000@gmail.com>
2021-01-07 17:43:18 +0700

    hoang

---
+++ Temp-Humid/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Temp_Humid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnHumid = new System.Windows.Forms.Button();
+            this.btnTemp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(203, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Do am";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(80, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Nhiet do";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // btnHumid
+            // 
+            this.btnHumid.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnHumid.Location = new System.Drawing.Point(299, 137);
+            this.btnHumid.Name = "btnHumid";
+            this.btnHumid.Size = new System.Drawing.Size(438, 71);
+            this.btnHumid.TabIndex = 2;
+            this.btnHumid.UseVisualStyleBackColor = false;
+            // 
+            // btnTemp
+            // 
+            this.btnTemp.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnTemp.Location = new System.Drawing.Point(299, 31);
+            this.btnTemp.Name = "btnTemp";
+            this.btnTemp.Size = new System.Drawing.Size(438, 71);
+            this.btnTemp.TabIndex = 3;
+            this.btnTemp.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTemp);
+            this.Controls.Add(this.btnHumid);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnHumid;
+        private System.Windows.Forms.Button btnTemp;
+    }
+}
+
---
+++ Temp-Humid/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace Temp_Humid
+{
+    public partial class Form1 : Form
+    {
+        private string strData;
+        public Form1()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(10);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                String Str = "https://api.thingspeak.com/channels/";
+                String End = "/feeds.json?results=1";
+                String Mid = "1238163";  //Channel ID Go's Here - Must be set to public
+
+                String All1 = String.Join(Mid, Str, End);
+
+
+                WebRequest request = WebRequest.Create(All1);
+                HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
+                Stream datastream = responce.GetResponseStream();
+                StreamReader reader = new StreamReader(datastream);
+                strData = reader.ReadToEnd();
+            }
+            catch (Exception)
+            {
+
+
+            }
+
+            //MessageBox.Show(strData);
+
+            try
+            {
+             
+
+                int start = strData.LastIndexOf("field1");
+                btnTemp.Text= strData.Substring(start+9,5);
+
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                int start2 = strData.LastIndexOf("field2");
+                btnHumid.Text = strData.Substring(start2 + 9,5);
+
+            }
+            catch (Exception)
+            {
+
+            }
+
+            //try
+            //{
+            //    int start3 = strData.LastIndexOf("field3");
+            //    Segments3 = strData.Substring(start3 + 9, 2);
+
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    int start4 = strData.LastIndexOf("field4");
+            //    Segments4 = strData.Substring(start4 + 9, 2);
+
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    int start5 = strData.LastIndexOf("field5");
+            //    Segments5 = strData.Substring(start5 + 9, 2);
+
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    int start6 = strData.LastIndexOf("field6");
+            //    Segments6 = strData.Substring(start6 + 9, 2);
+
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    int start7 = strData.LastIndexOf("field7");
+            //    Segments7 = strData.Substring(start7 + 9, 2);
+
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    int start8 = strData.LastIndexOf("field8");
+            //    Segments8 = strData.Substring(start8 + 9, 8);
+            //}
+            //catch (Exception)
+            //{
+
+
+            //}
+
+            //try
+            //{
+            //    var convertDouble = Convert.ToDouble(Segments);
+            //    Field1.Value = convertDouble;
+
+
+            //}
+            //catch (Exception)
+            //{
+
+            //}
+
+            //try
+            //{
+            //    var convertDouble2 = Convert.ToDouble(Segments2);
+            //    Field2.Value = convertDouble2;
+
+            //}
+            //catch (Exception)
+            //{
+
+            //}
+
+            //try
+            //{
+            //    var convertDouble3 = Convert.ToDouble(Segments3);
+            //    Field3.Value = convertDouble3;
+
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //try
+            //{
+            //    var convertDouble4 = Convert.ToDouble(Segments4);
+            //    Field4.Value = convertDouble4;
+
+            //}
+            //catch (Exception)
+            //{
+
+            //    throw;
+            //}
+
+            //try
+            //{
+            //    var convertDouble5 = Convert.ToDouble(Segments5);
+            //    Field5.Value = convertDouble5;
+
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //try
+            //{
+            //    var convertDouble6 = Convert.ToDouble(Segments6);
+            //    Field6.Value = convertDouble6;
+
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //try
+            //{
+            //    var convertDouble7 = Convert.ToDouble(Segments7);
+            //    Field7.Value = convertDouble7;
+
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+            //try
+            //{
+            //    var convertDouble8 = Convert.ToDouble(Segments8);
+            //    Field8.Value = convertDouble8;
+            //}
+            //catch (Exception)
+            //{
+            //}
+
+        }
+    }
+}
+    
